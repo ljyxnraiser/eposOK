f00aabe1b5c4afc9ff4a7932ee308991bdb8b10e
weyweywey <18388169715@163.com>
2019-02-01 22:29:31 +0800

    Default Changelist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">内幕牛</div>
+                <div class="title">里老奴坎坎坷坷</div>
             </div>
         </div>
     </body>
