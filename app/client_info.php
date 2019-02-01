371e713ca4094784a277b5b7209030a99d26605f
weyweywey <18388169715@163.com>
2019-02-01 23:11:47 +0800

    客户控制器

--- app/client_info.php
+++ app/client_info.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class client_info extends Model
 {
     protected $table ='client_info';
     protected $primaryKey='clent_id';
+
+    public function postRegister(Request $request){
+        $client_info=$request->get('client_info');
+
+    }
 }
