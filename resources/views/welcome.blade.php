54a381c19faf0f2fbf96f95755110b17c1867e8a
weyweywey <18388169715@163.com>
2019-02-01 22:54:24 +0800

    Default Changelist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">里老奴坎坎坷坷</div>
+                <div class="title">再来一次</div>
             </div>
         </div>
     </body>
